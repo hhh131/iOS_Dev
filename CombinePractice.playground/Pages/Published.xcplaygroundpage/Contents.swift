887218487bdc045b27c65e47fd25a4ebb7544685
hhh131 <gmlrnjs131@naver.com>
2023-02-08 02:39:45 +0900

    Foundation And Combine

--- CombinePractice.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ CombinePractice.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,7 +4,33 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "Jack"
+    var age: Int = 20
+}
+
+final class Label {
+    var text: String = ""
+    var num: Int = 0
+}
+
+let label = Label()
+let vm = SomeViewModel()
+
+print("Text : \(label.text)")
+
+vm.$name.assign(to: \.text, on: label) //
+vm.age = 100
+label.num = vm.age //!Published일 경우 값 변경시 직접 값을 재할당을 통해 값을 변경 해줘야 한다.
+
+print("age : \(label.num)")
+
+print("Text : \(label.text)")
+
+vm.name = "Jason"
+print("Text : \(label.text)")
 
 
-
+vm.name = "Hoo"
+print("Text : \(label.text)")
 //: [Next](@next)
--- CombinePractice.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ CombinePractice.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,19 @@
 import Foundation
 import Combine
 
+let subject = PassthroughSubject<String,Never>()
 
 
 
 // The print() operator prints you all lifecycle events
+let subscription = subject.print().sink { value in
+    print("Subscriber received value \(value)")
+}
 
-
+subject.send("hello")
+subject.send("hello again")
+subject.send("asdasdjkl")
+//subject.send(completion: .finished)
+subscription.cancel()
+subject.send("Asdasds")
 //: [Next](@next)
