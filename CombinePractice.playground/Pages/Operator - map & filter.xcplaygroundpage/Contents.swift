dcd4b0b1f57c4e16ff742b92fb9a6c75bd6141b3
hhh131 <gmlrnjs131@naver.com>
2023-02-09 23:36:42 +0900

    Scheduler & Operator(map&filter) Add.

--- CombinePractice.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ CombinePractice.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,31 @@ import Foundation
 import Combine
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int,Never>()
+let subscription1 = numPublisher
+    .map{ $0 * 2}
+    .sink{
+        value in
+        print("Transfoms Value: \(value)")
+    }
+
+numPublisher.send(10)
+numPublisher.send(30)
+numPublisher.send(20)
 
 
 // Filter
-
-
+let stringPubliser = PassthroughSubject<String,Never>()
+let subscription2 = stringPubliser
+    .filter{
+        $0.contains("a")
+    }
+    .sink { value in
+        print("Flitered value \(value)")
+    }
+stringPubliser.send("abc")
+stringPubliser.send("dsd")
+stringPubliser.send("Jack")
+stringPubliser.send("John")
+subscription2.cancel()
 //: [Next](@next)
